0bad5b366dd89d9256a26a70b7675175648fe398
marioavolio <marioavolio1999@gmail.com>
2021-08-03 12:52:13 +0200

    remove LICENSE.txt

--- AI/src/costants.py
+++ AI/src/costants.py
@@ -1,6 +1,6 @@
 import os
 
-SRC_PATH = os.path.dirname(__file__)  # Where your .py file is locateds.path.join(SETUP_PATH, 'src')
+SRC_PATH = os.path.dirname(__file__)  # Where your .py file is located
 RESOURCES_PATH = os.path.join(SRC_PATH, 'resources')
 DLV_PATH = os.path.join(RESOURCES_PATH, 'dlv')
 SPRITE_PATH = os.path.join(RESOURCES_PATH, 'sprites')
