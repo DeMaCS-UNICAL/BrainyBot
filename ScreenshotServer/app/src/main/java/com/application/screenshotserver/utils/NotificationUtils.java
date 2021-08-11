9d1af0fbe7d456321e7068a6cd93e8d6da30c165
MarioAvolio <marioavolio1999@gmail.com>
2021-08-11 16:02:08 +0200

    upgrade notification

--- ScreenshotServer/app/src/main/java/com/application/screenshotserver/utils/NotificationUtils.java
+++ ScreenshotServer/app/src/main/java/com/application/screenshotserver/utils/NotificationUtils.java
@@ -15,23 +15,29 @@ import androidx.core.util.Pair;
 import com.application.screenshotserver.R;
 
 
+// from https://developer.android.com/training/notify-user/build-notification
 public class NotificationUtils {
 
     public static final int NOTIFICATION_ID = 1337;
-    private static final String NOTIFICATION_CHANNEL_ID = "com.application.tappingbot.app";
-    private static final String NOTIFICATION_CHANNEL_NAME = "com.application.tappingbot.app";
+    private static final String NOTIFICATION_CHANNEL_ID = "com.application.screenshotserver.app";
+    private static final String NOTIFICATION_CHANNEL_NAME = "com.application.screenshotserver.app";
 
     public static Pair<Integer, Notification> getNotification(@NonNull Context context) {
         createNotificationChannel(context);
         Notification notification = createNotification(context);
         NotificationManager notificationManager
                 = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
+
+
+        // notificationId is a unique int for each notification that you must define
         notificationManager.notify(NOTIFICATION_ID, notification);
         return new Pair<>(NOTIFICATION_ID, notification);
     }
 
     @TargetApi(Build.VERSION_CODES.O)
     private static void createNotificationChannel(@NonNull Context context) {
+        // Create the NotificationChannel, but only on API 26+ because
+        // the NotificationChannel class is new and not in the support library
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
             NotificationChannel channel = new NotificationChannel(
                     NOTIFICATION_CHANNEL_ID,
@@ -39,6 +45,9 @@ public class NotificationUtils {
                     NotificationManager.IMPORTANCE_LOW
             );
             channel.setLockscreenVisibility(Notification.VISIBILITY_PRIVATE);
+
+            // Register the channel with the system; you can't change the importance
+            // or other notification behaviors after this
             NotificationManager manager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
             manager.createNotificationChannel(channel);
         }
