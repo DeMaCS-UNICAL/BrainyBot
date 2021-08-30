bf7dcfaa83e3604744678a31c9b207f1a34a9090
Mario <marioavolio1999@gmail.com>
2021-08-30 18:02:22 +0200

    modify example.py

--- AI/examples/example.py
+++ AI/examples/example.py
@@ -2,7 +2,7 @@ import cv2
 from matplotlib import pyplot as plt
 
 from src.candygraph.candygraph import CandyGraph
-from src.candygraph.constants import PX, PY
+from src.candygraph.constants import PX, PY, TYPE
 from src.detect.detect import MatchingCandy
 from src.dlvsolution.dlvsolution import DLVSolution
 from src.dlvsolution.helpers import getInputDLVNodes, getEdges, Swap
@@ -10,29 +10,73 @@ from src.webservices.helpers import makeJson, requireImageFromURL
 
 
 def draw(matrixCopy, nodes, color):
-    width, height = 70, 70
-    cv2.rectangle(matrixCopy, (nodes[PX] + 35, nodes[PY] + 35), (nodes[PX] + width, nodes[PY] + height),
+    width, height = 110, 110
+    cv2.rectangle(matrixCopy, (nodes[PX], nodes[PY]), (nodes[PX] + width, nodes[PY] + height),
                   color,
                   10)
+
+
+RED = "red"
+BLUE = "blue"
+YELLOW = "yellow"
+GREEN = "green"
+PURPLE = "purple"
+ORANGE = "orange"
+WHITE = "white"
+nameColor = {
+    RED: (255, 0, 0),
+    BLUE: (0, 0, 255),
+    YELLOW: (255, 255, 0),
+    GREEN: (0, 255, 0),
+    PURPLE: (128, 0, 128),
+    ORANGE: (255, 165, 0),
+    WHITE: (255, 255, 255)
+}
+
+
+def getColor(str) -> ():
+    # print(str)
+    name = None
+    for color in [RED, BLUE, YELLOW, GREEN, PURPLE, ORANGE]:
+        if color in str:
+            name = color
+
+    if "colourB" in str:
+        name = WHITE
+
+    if name in nameColor:
+        return nameColor[name]
+
+    return nameColor[RED]
 
 
 def main():
     # require image from server
     # TODO: change ip!
-    serverIp, port = "192.168.1.58", 5432
-    requireImageFromURL(serverIp, port)
-    print("requireImageFromURL check")
+    serverIp, port = "192.168.1.37", 5432
+    try:
+        requireImageFromURL(serverIp, port)
+        print("REQUIRE OK!")
+    except Exception as e:
+        print(e)
 
     # execute template matching
     spriteSize = (110, 110)
     matchingCandy = MatchingCandy(spriteSize)
-    tmp = matchingCandy.getMatrix().copy()
-    plt.imshow(tmp)
-    plt.title(f"Screenshot acquired")
+    matrixCopy = matchingCandy.getMatrix().copy()  # copy img
+    plt.imshow(matrixCopy)
+    plt.title(f"Screenshot")
     plt.show()
 
     # take graph
     candyGraph: CandyGraph = matchingCandy.search()
+    for node in candyGraph.getNodes():
+        color = getColor(node[TYPE])
+        draw(matrixCopy, node, color)
+
+    plt.imshow(matrixCopy)
+    plt.title(f"Matching")
+    plt.show()
 
     # get nodes and edges of graph for DLV
     nodesAndInformation = getInputDLVNodes(candyGraph)
@@ -50,8 +94,8 @@ def main():
     node1 = candyGraph.getNode(swap.get_id1())
     node2 = candyGraph.getNode(swap.get_id2())
 
-    draw(tmp, node1, (255, 255, 255))
-    draw(tmp, node2, (255, 255, 255))
+    draw(tmp, node1, nameColor[WHITE])
+    draw(tmp, node2, nameColor[WHITE])
     plt.imshow(tmp)
     plt.title(f"OPTIMUM {node1} --> {node2}.")
     plt.show()
