76997ba23a9d62d51304d7cc5005896a116c11ac
marioavolio <marioavolio1999@gmail.com>
2021-08-30 19:41:46 +0200

    insert installer.py

---
+++ AI/src/resources/installer.py
@@ -0,0 +1,19 @@
+import os
+
+import pip
+
+from src.constants import RESOURCES_PATH
+
+
+def install_whl(path):
+    pip.main(['install', path])
+
+
+def main():
+    install_whl(os.path.join(RESOURCES_PATH, "EmbASP-7.4.0-py2.py3-none-any.whl"))  # EMBASP INSTALLER
+
+
+if __name__ == '__main__':
+    main()
+
+# RUN TO INSTALL EMBASP
