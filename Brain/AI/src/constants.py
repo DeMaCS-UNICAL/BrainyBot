578088d6ea6a6fb280a97b7f78c7616899489b44
Agelin Bee <ianni@Maia>
2023-03-25 23:23:13 +0100

    -Fixed path for tappy-client scripts

--- Brain/AI/src/constants.py
+++ Brain/AI/src/constants.py
@@ -3,8 +3,8 @@ import os
 SRC_PATH = os.path.dirname(__file__)  # Where your .py file is located
 RESOURCES_PATH = os.path.join(SRC_PATH, 'resources')
 SCREENSHOT_PATH = os.path.join(RESOURCES_PATH, 'screenshot')
-CLIENT_PATH = os.path.join(SRC_PATH, '../../tapsterbot-original/clients/python')
+CLIENT_PATH = os.path.join(SRC_PATH, '../../../tapsterbot-original/clients/python')
 DLV_PATH = os.path.join(RESOURCES_PATH, 'dlv')
-# TODO: change IP!
-SCREENSHOT_SERVER_IP = '192.168.0.51'
-TAPPY_ORIGINAL_SERVER_IP = '192.168.0.3'
+# change IP addresses to your needs.
+SCREENSHOT_SERVER_IP = '192.168.0.51'     # IP of the mobile phone with Screenshotserver on board
+TAPPY_ORIGINAL_SERVER_IP = '192.168.0.3'  # IP of the server where the robot is attached to
