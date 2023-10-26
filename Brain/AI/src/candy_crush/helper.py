0eb45ac9551039bbc9379a759e7119f45ac464ab
deniseAngilica <deangilica@gmail.com>
2023-10-26 14:58:34 +0200

    candy_crush playing in loop with feedback

--- Brain/AI/src/candy_crush/helper.py
+++ Brain/AI/src/candy_crush/helper.py
@@ -8,122 +8,96 @@ from matplotlib import pyplot as plt
 from AI.src.abstraction.object_graph import ObjectGraph
 from AI.src.candy_crush.object_graph.constants import PX, PY, TYPE
 from AI.src.candy_crush.constants import RED, YELLOW, PURPLE, GREEN, BLUE, WHITE, nameColor, ORANGE
-from AI.src.candy_crush.detect.new_detect import MatchingCandy
+from AI.src.candy_crush.detect.new_detect import MatchingCandy,draw
 from AI.src.candy_crush.dlvsolution.dlvsolution import DLVSolution
 from AI.src.candy_crush.dlvsolution.helpers import get_input_dlv_nodes, get_edges, Swap
 from AI.src.constants import CLIENT_PATH, TAPPY_ORIGINAL_SERVER_IP
+from AI.src.vision.feedback import Feedback
 
 
-def draw(matrixCopy, nodes, color):
-    width, height = 110, 110
-    cv.rectangle(matrixCopy, (nodes[PX], nodes[PY]), (nodes[PX] + width, nodes[PY] + height), color, 10)
 
-
-def get_color(strg) -> ():
-    # print(strg)
-    name = None
-    for color in [RED, BLUE, YELLOW, GREEN, PURPLE, ORANGE]:
-        if color in strg:
-            name = color
-
-    if "colourB" in strg:
-        name = WHITE
-
-    if name in nameColor:
-        return nameColor[name]
-
-    return nameColor[RED]
-
+def asp_input(graph):
+    to_return = get_input_dlv_nodes(graph)
+    to_return.extend(get_edges(graph))
+    return to_return
 
 def candy_crush(screenshot,debug = False):
     # execute template matching
     spriteSize = (110, 110)
     matchingCandy = MatchingCandy(screenshot,spriteSize,debug)
-    matrixCopy = matchingCandy.get_matrix().copy()  # copy img
-
     if not debug:
         plt.ion()
+    '''
+    
 
     plt.imshow(matrixCopy)
     plt.title(f"Screenshot")
     plt.show()
     if not debug:
         plt.pause(0.1)
-
+    '''
 
     # take graph
     candyGraph: ObjectGraph = matchingCandy.search()
-    number_per_type={}
-    for node in candyGraph.get_nodes():
-        color = get_color(node[TYPE])
-        draw(matrixCopy, node, color)
-        if not node[TYPE] in number_per_type.keys():
-            number_per_type[node[TYPE]] = 0
-        number_per_type[node[TYPE]] = number_per_type[node[TYPE]]+1
-    for type in number_per_type.keys():
-        print(f"{type[0:-4]}:{number_per_type[type]}",file=sys.stderr,end='\t')
-    print("",file=sys.stderr)
-    plt.imshow(matrixCopy)
-    plt.title(f"Matching")
-    plt.show()
-    if not debug: 
-        plt.pause(0.5)
+   
 
 
     # get nodes and edges of graph for DLV
-    nodesAndInformation = get_input_dlv_nodes(candyGraph)
-    edges = get_edges(candyGraph)
+    input = asp_input(candyGraph)
 
-
+    success = True
     #print(f"EDGES --> {edges}")
     #print()
     #print(f"NODES --> {nodesAndInformation}")
     if(debug):
         return
 
-    # recall ASP program
-    solution = DLVSolution()
-    swap: Swap = solution.recall_asp(edges, nodesAndInformation)
-
-    if swap is None:
-        print("No moves found. Maybe there is no candy on screen?")
-    else:
-        # draw
-        tmp = matchingCandy.get_matrix().copy()
-        node1 = candyGraph.get_node(swap.get_id1())
-        node2 = candyGraph.get_node(swap.get_id2())
-
-        draw(tmp, node1, nameColor[WHITE])
-        draw(tmp, node2, nameColor[WHITE])
-        plt.imshow(tmp)
-        plt.title(f"OPTIMUM {node1} --> {node2}.")
-        plt.show()
-        if not debug:
-            plt.pause(0.5)
-
-
-        #
-        # Enlarges swipe coordinates so to start swiping not from the center of the candy but from the border
-        #
-        width, height = 110, 110
-        x1,y1,x2,y2 = node1[PX], node1[PY], node2[PX], node2[PY]
-        EL = 20  #pixels of swipe offset
-        
-        if (abs(x1-x2) < 10):
-	    #swipe vertical
-            SX1 = int( (x1+x2)/2+width/2 )
-            SX2 = SX1
-            SY1 = int(min(y1,y2)+EL) 
-            SY2 = int(max(y1,y2) + height+EL) 
+    while success:
+        # recall ASP program
+        solution = DLVSolution()
+        swap1,answer_set = solution.recall_asp(input)
+        swap: Swap = swap1
+        if swap is None:
+            print("No moves found. Maybe there is no candy on screen?")
         else:
-        # assumiamo swipe orizzontale
-            SY1 = int((y1+y2)/2+height/2)
-            SY2 = int(SY1)
-            SX1 = int(min(x1,x2)+EL) 
-            SX2 = int(max(x1,x2) + width + EL)  
+            # draw
+            tmp = matchingCandy.get_matrix().copy()
+            node1 = candyGraph.get_node(swap.get_id1())
+            node2 = candyGraph.get_node(swap.get_id2())
+
+            draw(tmp, node1, nameColor[WHITE])
+            draw(tmp, node2, nameColor[WHITE])
+            plt.imshow(tmp)
+            plt.title(f"OPTIMUM {node1} --> {node2}.")
+            plt.show()
+            if not debug:
+                plt.pause(0.5)
 
 
-        os.chdir(CLIENT_PATH)
-        os.system(f"python3 client3.py --url http://{TAPPY_ORIGINAL_SERVER_IP}:8000 --light 'swipe {SX1} {SY1} {SX2} {SY2}'")
+            #
+            # Enlarges swipe coordinates so to start swiping not from the center of the candy but from the border
+            #
+            width, height = 110, 110
+            x1,y1,x2,y2 = node1[PX], node1[PY], node2[PX], node2[PY]
+            EL = 20  #pixels of swipe offset
+            
+            if (abs(x1-x2) < 10):
+            #swipe vertical
+                SX1 = int( (x1+x2)/2+width/2 )
+                SX2 = SX1
+                SY1 = int(min(y1,y2)+EL) 
+                SY2 = int(max(y1,y2) + height+EL) 
+            else:
+            # assumiamo swipe orizzontale
+                SY1 = int((y1+y2)/2+height/2)
+                SY2 = int(SY1)
+                SX1 = int(min(x1,x2)+EL) 
+                SX2 = int(max(x1,x2) + width + EL)  
+
+
+            os.chdir(CLIENT_PATH)
+            os.system(f"python3 client3.py --url http://{TAPPY_ORIGINAL_SERVER_IP}:8000 --light 'swipe {SX1} {SY1} {SX2} {SY2}'")
+            feedback = Feedback()
+            success,candyGraph,input = feedback.request_feedback(matchingCandy.vision,matchingCandy.abstraction,asp_input,answer_set)
     
     
--- Brain/AI/src/vision/feedback.py
+++ Brain/AI/src/vision/feedback.py
@@ -1,41 +1,52 @@
 import asyncio
 import cv2
+import os
 import numpy as np
-from languages.mapper import Mapper
+from languages.asp.asp_mapper import ASPMapper
 from languages.predicate import Predicate
 from time import sleep
+from AI.src.webservices.helpers import getScreenshot
+from AI.src.abstraction.helpers import getImg
+from AI.src.constants import SCREENSHOT_PATH, SCREENSHOT_FILENAME
+import constants
+from matplotlib import pyplot as plt
+
 class Feedback:
-    feedbackers=[]
     def __init__(self, tolerance=0):
-        self.current_invoker=None
         self.current_vision_callback=None
-        self.current_expected_result=None
+        self.current_expected_result=[]
+        self.current_abstraction_callback = None
         self.tolerance = tolerance
-        self.mapper=Mapper()
-        Feedback.feedbackers.append(self)
+        self.mapper=ASPMapper.get_instance()
     
-    #request async or request+send back? 
-    def request_feedback(self, invoker, vision_callback:function, abstraction_callback:function,expected_result:()):
-        self.current_invoker=invoker
-        self.current_expected_result = expected_result
+    def request_feedback(self, vision_callback, abstraction_callback, abstraction_to_asp,expected_result):
+        self.current_expected_result = []
         self.current_vision_callback = vision_callback
         self.current_abstraction_callback = abstraction_callback
+        self.current_abstraction_to_asp_callback = abstraction_to_asp
+        for obj in expected_result.get_answer_set():
+            if(obj.startswith('feedback_')):
+                self.current_expected_result.append(obj[8:])
         return self.main()
     
     
     def take_screenshot(self):
-        pass
+        server_ip, port = constants.SCREENSHOT_SERVER_IP, 5432
+        try:
+            getScreenshot(server_ip, port)
+            print("SCREENSHOT TAKEN.")
+        except Exception as e:
+            print(e)
+            exit(1)
 
     def compare_with_expected(self,actual)->bool:
-        return np.all(np.isin(self.current_expected_result, actual))
+        to_return= np.all(np.isin(self.current_expected_result, actual))
+        print("success? ",to_return)
+        self.current_expected_result=[]
+        return to_return
 
-    def board_is_stable(self, img1, img2):
-        if img1 == img2:
-            return True
-        difference = cv2.subtract(img1, img2)
-        b, g, r = cv2.split(difference)
-
-        if cv2.countNonZero(b) <= self.tolerance and cv2.countNonZero(g) <= self.tolerance and cv2.countNonZero(r) <= self.tolerance:
+    def board_is_stable(self, abstr1, abstr2):
+        if np.array_equal(abstr1,abstr2):
             return True
         return False
 
@@ -47,15 +58,20 @@ class Feedback:
         return to_return
 
     def main(self):
-        screenshot_to_compare1 = self.take_screenshot()
+        self.take_screenshot()
+        vision_result = self.current_vision_callback()
+        abstraction_result = self.current_abstraction_callback(vision_result)
+        asp_input=self.current_abstraction_to_asp_callback(abstraction_result)
+        mapped_objects1 = self.get_mapped(asp_input)
         while True:
-            screenshot_to_compare2 = self.take_screenshot()
-            if not self.board_is_stable(screenshot_to_compare1,screenshot_to_compare2):
-                screenshot_to_compare1 = screenshot_to_compare2
-                sleep(0.5)
+            self.take_screenshot()
+            vision_result = self.current_vision_callback()
+            abstraction_result = self.current_abstraction_callback(vision_result)
+            asp_input=self.current_abstraction_to_asp_callback(abstraction_result)
+            mapped_objects2 = self.get_mapped(asp_input)
+            if not self.board_is_stable(mapped_objects1,mapped_objects2):
+                mapped_objects1 = mapped_objects2
+                sleep(0.1)
             else:
-                vision_result = self.current_vision_callback()
-                abstraction_result = self.current_abstraction_callback(vision_result)
-                mapped_objects = self.get_mapped(abstraction_result)
-                return (self.compare_with_expected(mapped_objects),abstraction_result)
+                return (self.compare_with_expected(mapped_objects2),abstraction_result,asp_input)
    
